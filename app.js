5fc6f427b48f169bb1687db7f6073c883d1951cd
veenavenkatreddy <venkatrv@roehampton.ac.uk>
2025-08-06 22:42:16 +0100

    intial code

---
+++ app.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const app = express();
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// View Engine
+app.set('view engine', 'pug');
+app.set('views',path.join(__dirname, 'views'));
+
+// Routes
+const indexRoutes = require('./routes/index');
+app.use('/', indexRoutes);
+
+// Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
