6ddc087d41fa6ca67ad1e1d767061290bbf13a1c
veenavenkatreddy <venkatrv@roehampton.ac.uk>
2025-08-08 00:28:52 +0100

    database loaded to server

--- config/database.js
+++ config/database.js
@@ -2,16 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-    
-    // Create default categories on first connection
-    const Category = require('../models/categoryModel');
-    await Category.createDefaultCategories();
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
+    console.error(`Mongo Error`, error);
+    process.exit(1); // Exit process if DB connection fails
   }
 };
 
